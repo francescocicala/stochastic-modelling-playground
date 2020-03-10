70f14734d05efed71b89ecc9942352bcd7d11c5e
Francesco <francesco.cicala00@gmail.com>
2020-03-10 23:23:51 +0100

    fix error

--- evolve.py
+++ evolve.py
@@ -1,8 +1,8 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
-### Evolution Tools ###
 
+### Evolution Tools ###
 def evolve(state, deriv, length, **kwargs):
     history = np.zeros((length, len(state)))
     history[0] = state
@@ -15,14 +15,18 @@ def evolve(state, deriv, length, **kwargs):
 
 ### Visualization Tools ###
 def print_args(**kwargs):
+    print("Parameters: ")
     for key, item in kwargs.items():
         print('{} : {}'.format(key, item))
 
-def show(history, legend=None, title=None, **kwargs):
-    print_args(**kwargs)
-    plt.plot(history)
+
+def show(history, legend=None, title=None, first_t=0, last_t=-1, **kwargs):
+    plt.plot(history[first_t : last_t])
+
     if legend is not None:
         plt.legend(legend)
+
     if title is not None:
         plt.title(title)
+
     plt.show()
